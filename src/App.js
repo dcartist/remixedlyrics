0a02797c0e32741c32a6f12a0b1806d737e0c9e8
dcartist <pbannerman13@gmail.com>
2023-01-07 21:34:31 -0500

    updating packages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
+import logo from './Images/more-coffee.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-   
+   <img src={logo} class="img-fluid rounded-top" alt=""/>
     </div>
   );
 }
