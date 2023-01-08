988fe776d34dab573313677273c170d32010e4e0
dcartist <pbannerman13@gmail.com>
2023-01-07 21:47:19 -0500

    pushing tagline

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
    <img src={logo} class="img-fluid rounded-top" alt=""/>
+   <p> where coffee and tea can make music better!</p>
     </div>
   );
 }
