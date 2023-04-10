167fe13efcfc43060585eb98d55328c815afafca
dcartist <pbannerman13@gmail.com>
2023-04-09 22:00:55 -0400

    updating package

--- src/App.js
+++ src/App.js
@@ -2,6 +2,15 @@
 import RemixedCoffeeLogo from './Images/RemixedCoffeLogo';
 import Navigation from './Components/Navigation/Navigtation';
 import './App.scss';
+import * as prismic from "@prismicio/client";
+const client = prismic.createClient("remixedPrismic"); 
+const repoName = "remixedPrismic";
+const endpoint = prismic.getEndpoint(repoName);
+// const client = prismic.createClient(endpoint);
+console.log(client.endpoint);
+console.log(endpoint);
+const blogPosts =  client.getAllByType("song");
+console.log(blogPosts);
 
 function App() {
   return (
